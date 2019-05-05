a92e3a92dd25e79a0552b9d2e7f57704b49390bd
Eric Kfir <eric@kfirs.com>
2019-05-05 20:25:52 +0300

    Add transactions in "init.sql" when running in development modeˆ

--- assets/rdbms/init.sql
+++ assets/rdbms/init.sql
@@ -9,4 +9,10 @@ VALUES (1, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 
        (3, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'Loans', 1),
        (4, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'Salary', 1),
        (5, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'Insurance', 2),
-       (6, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'CheapAndHealthy Inc.', 5);
+       (6, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'CheapAndHealthy Inc.', 5),
+       (7, DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), NULL, NULL, 'SafeStruct Ltd.', 5);
+
+INSERT INTO bb.transactions (created_on, origin, source_account_id, target_account_id, amount, comments)
+VALUES (DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), 'Init', 4, 6, 100, 'Health insurance'),
+       (DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), 'Init', 4, 7, 35, 'Structure insurance'),
+       (DATE_ADD(DATE(NOW()), INTERVAL FLOOR(RAND() * 24) HOUR), 'Init', 4, 3, 1269, 'Pay loan 1');
