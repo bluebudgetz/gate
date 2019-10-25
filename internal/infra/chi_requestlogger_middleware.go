bb6fd0f7db8087a70cdee7e65dc5e53085714b70
Eric Kfir <eric@kfirs.com>
2019-10-25 14:02:32 +0300

    Support logging HTTP response in request log
    This is done by using Chi's "middleware.NewWrapResponseWriter" API.
    We only log the output for failed requests.

--- internal/infra/chi_requestlogger_middleware.go
+++ internal/infra/chi_requestlogger_middleware.go
@@ -1,31 +1,15 @@
 package infra
 
 import (
+	"bytes"
 	"net/http"
 	"runtime/debug"
 	"time"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
-
-type statusCodeSavingResponseWriter struct {
-	w          http.ResponseWriter
-	statusCode int
-}
-
-func (s *statusCodeSavingResponseWriter) Header() http.Header {
-	return s.w.Header()
-}
-
-func (s *statusCodeSavingResponseWriter) Write(bytes []byte) (int, error) {
-	return s.w.Write(bytes)
-}
-
-func (s *statusCodeSavingResponseWriter) WriteHeader(statusCode int) {
-	s.statusCode = statusCode
-	s.w.WriteHeader(statusCode)
-}
 
 func RequestLogger(next http.Handler) http.Handler {
 	zerologDictFromHeader := func(headers http.Header) *zerolog.Event {
@@ -48,15 +32,19 @@ func RequestLogger(next http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Use custom response writer that saves the status code (so we can log it)
-		ww := &statusCodeSavingResponseWriter{statusCode: -1, w: w}
+		// Wrap the response to enable access to the final status code & response contents
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+		// Tee the response to an additional buffer so we can look inside & log it
+		buf := bytes.Buffer{}
+		ww.Tee(&buf)
 
 		// Invoke target handler, but time the request
 		start := time.Now()
 		recovered, stack := requestLoggerInvoke(next, r, ww)
 		targetStopTime := time.Now()
 
-		// TODO: support logging actual request/response body
+		// Create the logging event; an Error event for panics, and Info event otherwise
 		var event *zerolog.Event
 		var message string
 		if recovered != nil {
@@ -66,7 +54,7 @@ func RequestLogger(next http.Handler) http.Handler {
 			} else {
 				event = event.Interface(zerolog.ErrorFieldName, recovered)
 			}
-			event = event.Str(zerolog.ErrorStackFieldName, string(stack))
+			event = event.Str(zerolog.ErrorStackFieldName, string(stack)).Bytes("out", buf.Bytes())
 			message = "HTTP request failed"
 		} else {
 			event = log.Info()
@@ -84,7 +72,8 @@ func RequestLogger(next http.Handler) http.Handler {
 			Dict("requestHeaders", zerologDictFromHeader(r.Header)).
 			Dict("responseHeaders", zerologDictFromHeader(w.Header())).
 			TimeDiff("elapsed", targetStopTime, start).
-			Int("status", ww.statusCode).
+			Int("bytesWritten", ww.BytesWritten()).
+			Int("status", ww.Status()).
 			Msg(message)
 	})
 }
